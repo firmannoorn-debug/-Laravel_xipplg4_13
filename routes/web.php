33575780d353100ede36fea745c5d61333631131
firmannoorn-debug <firmannoorn@gmail.com>
2025-10-21 15:30:07 +0700

    ika

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Landingcontroller;
+use App\Http\Controllers\Admin\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', [Landingcontroller::class,'index'])->name('Landing');
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [Dashboardcontroller::class, 'index'])->name('admin.dashboard');
 });
