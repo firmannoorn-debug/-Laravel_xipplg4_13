004bf9e80b6bcd901179e982461b5c9cbf79f0c6
firmannoorn-debug <firmannoorn@gmail.com>
2025-11-04 12:54:47 +0700

    add Feature

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Landingcontroller;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\StudentController;
 
+Route::get('/', [Landingcontroller::class, 'index'])->name('Landing');
 
-Route::get('/', [Landingcontroller::class,'index'])->name('Landing');
-
-Route::prefix('admin')->group(function () {
-    Route::get('/dashboard', [Dashboardcontroller::class, 'index'])->name('admin.dashboard');
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    Route::resource('students', StudentController::class);
 });
